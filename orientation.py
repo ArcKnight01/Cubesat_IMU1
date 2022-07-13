0e7039139deff10a7340ee18a03ee39f0a022b48
Grant <gsims@mit.edu>
2022-07-13 13:58:42 -0700

    updated python file

--- orientation.py
+++ orientation.py
@@ -3,19 +3,23 @@ import os
 import board
 import busio
 import adafruit_fxos8700
-
+import adafruit_fxas21002c
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_fxos8700.FXOS8700(i2c)
-
+fxos = adafruit_fxos8700.FXOS8700(i2c)
+fxas = adafruit_fxas21002c.FXAS21002C(i2c)
 
 while True:
-    accelX, accelY, accelZ = sensor.accelerometer
-    magnetX, magnetY, magnetZ = sensor.magnetic
-    gyroX, gyroY, gyroZ = sensor.gyro
-    gravityX, gravityY, gravityZ = sensor.gravity
+    accelX, accelY, accelZ = fxos.accelerometer
+    magnetX, magnetY, magnetZ = fxos.magnetometer
+    gyroX, gyroY, gyroZ = fxas.gyroscope
 
-    print('accelX':, accelX)
+
+    #gyroX, gyroY, gyroZ = sensor.gyro
+    #gravityX, gravityY, gravityZ = sensor.gravity
+
+    print('gyroX:', gyroX, ' gyroY:', gyroY, ' gyroZ:', gyroZ)
+    time.sleep(1)
 
 
 print('code runs')
