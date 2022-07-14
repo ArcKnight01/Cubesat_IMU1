1886856d7582cfc6496ace1cc7b19424706c3cd3
Nevin <nevint@mit.edu>
2022-07-14 02:15:42 +0100

    Added setup for gyroscope & reformatted function inputs

--- precision.py
+++ precision.py
@@ -3,17 +3,20 @@ import os
 import board
 import busio
 import adafruit_fxos8700
+import adafruit_fxas21002c
 
 i2c = busio.I2C(board.SCL, board.SDA)
-sensor = adafruit_fxos8700.FXOS8700(i2c)
+accelerometer = adafruit_fxos8700.FXOS8700(i2c)
+gyroscope = adafruit_fxas21002c.FXAS21002C(i2c)
 
-def checkPrecision(testSensor, sampleLength):
+def checkPrecision(testAccel, testGyro, sampleLength):
     """This function checks the precision of the accelerometer/magnometer
     connected to the Raspberry Pi. Leave the sensor on a flat surface 
     and do not disturb it while the test is running.
 
     Args:
-        testSensor: The sensor that is being tested. Must be anadafruit_fxos8700.FXOS8700 sensor object.
+        testAccel: The accelerometer that is being tested. Must be anadafruit_fxos8700.FXOS8700 sensor object.
+        testGyro: The gyroscope that is being tested. Must be adafruit_fxas21002c.FXAS21002C sensor object.
         sampleLength: The ammount of time for which the test should be carried out in seconds. Must be an integer. 
     
     Returns:
@@ -24,15 +27,19 @@ def checkPrecision(testSensor, sampleLength):
     Raises:
         AssertionError: Raised when incorrect sensor object is inputted or sample time in not an integer
     """
-    assert type(testSensor) is adafruit_fxos8700.FXOS8700 and type(sampleLength) is int
+    assert type(testAccel) is adafruit_fxos8700.FXOS8700 and type(testGyro) is adafruit_fxas21002c.FXAS21002C and type(sampleLength) is int
+
+    aXlist, aYlist, aZlist, gXlist, gYlist, gZlist = ([] for i in range(6))
+    sensVals = [accelX,accelY,accelZ,gyroX,gyroY,gyroZ]
 
     end = time.time_ns() + sampleLength*1000000000
     
     while time.time_ns() < end:
-        accelX, accelY, accelZ = testSensor.accelerometer
-    print(accelX)
+        accelX, accelY, accelZ = testAccel.accelerometer
+        gyroX, gyroY, gyroZ = testGyro.gyroscope
+    print(aXlist)
 
-checkPrecision(sensor,5)
+checkPrecision(accelerometer,gyroscope,5)
 
     
 
